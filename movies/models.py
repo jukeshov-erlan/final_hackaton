ce1e69790240729ef35f71e5468f97a7f6f090fe
test <test@gmail.com>
2024-03-11 17:29:54 +0600

    docker

--- movies/models.py
+++ movies/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from slugify import slugify
 from datetime import datetime
 
+
 class Category(models.Model):
     slug = models.SlugField(max_length=160, unique=True, primary_key=True, blank=True)
     name = models.CharField('Категория', max_length=150)
@@ -13,7 +14,7 @@ class Category(models.Model):
 
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
@@ -33,7 +34,7 @@ class Actor(models.Model):
 
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
         verbose_name = 'Актеры и режиссеры'
         verbose_name_plural = 'Актеры и режиссеры'
@@ -48,10 +49,9 @@ class Genre(models.Model):
             self.slug = slugify(self.name)
         super().save()
 
-
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
@@ -69,8 +69,10 @@ class Movie(models.Model):
     actors = models.ManyToManyField(Actor, verbose_name='Актеры', related_name='film_actor')
     genres = models.ManyToManyField(Genre, verbose_name='Жанры')
     budget = models.PositiveIntegerField(verbose_name='Бюджет', default=0, help_text='указать сумму в долларах')
-    fees_in_usa = models.PositiveIntegerField(verbose_name='Сборы в США', default=0, help_text='указать сумму в долларах')
-    fees_in_world = models.PositiveIntegerField(verbose_name='Сборы в мире', default=0, help_text='указать сумму в долларах')
+    fees_in_usa = models.PositiveIntegerField(verbose_name='Сборы в США', default=0,
+                                              help_text='указать сумму в долларах')
+    fees_in_world = models.PositiveIntegerField(verbose_name='Сборы в мире', default=0,
+                                                help_text='указать сумму в долларах')
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
     draft = models.BooleanField(verbose_name='Черновик', default=False)
 
@@ -79,17 +81,16 @@ class Movie(models.Model):
             self.slug = slugify(self.title)
         super().save()
 
-
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         verbose_name = 'Фильм'
         verbose_name_plural = 'Фильмы'
 
 
 class MovieShots(models.Model):
-    title= models.CharField(verbose_name='Заголовок', max_length=100)
+    title = models.CharField(verbose_name='Заголовок', max_length=100)
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(verbose_name='Изображение', upload_to='movie_shots/')
     movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
@@ -107,7 +108,7 @@ class RatingStar(models.Model):
 
     def __str__(self) -> str:
         return str(self.value)
-    
+
     class Meta:
         verbose_name = 'Звезда рейтинга'
         verbose_name_plural = 'Звезды рейтинга'
@@ -120,7 +121,7 @@ class Rating(models.Model):
 
     def __str__(self) -> str:
         return f'{self.user} - {self.star} - {self.movie}'
-    
+
     class Meta:
         verbose_name = 'Рейтинг'
         verbose_name_plural = 'Рейтинги'
@@ -130,13 +131,13 @@ class Review(models.Model):
     email = models.EmailField()
     name = models.CharField('Имя', max_length=100)
     text = models.TextField('Сообщение', max_length=5000)
-    parent = models.ForeignKey('self', verbose_name='Родитель', on_delete=models.SET_NULL, blank=True, null=True, related_name='children')
+    parent = models.ForeignKey('self', verbose_name='Родитель', on_delete=models.SET_NULL, blank=True, null=True,
+                               related_name='children')
     movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self) -> str:
         return f'{self.name} - {self.movie}'
-    
+
     class Meta:
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
-
